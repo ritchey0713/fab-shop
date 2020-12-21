46c6268783cf2fbf3681488d30f40d465e235939
Dj <ritchey0713@gmail.com>
2020-12-21 12:45:59 -0500

    load provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import App from "./comoponents/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
