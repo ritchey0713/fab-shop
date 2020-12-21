aad7814f0c2d6a2f14cfae0346e8eac2984fa142
Dj <ritchey0713@gmail.com>
2020-12-21 12:52:23 -0500

    render provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-
+import store from "./redux/store";
 import App from "./comoponents/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
