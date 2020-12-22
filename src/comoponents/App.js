b33a883808ce4703123030ba4cfcbd59abe7bea2
Dj <ritchey0713@gmail.com>
2020-12-22 14:26:42 -0500

    add basic routing

--- src/comoponents/App.js
+++ src/comoponents/App.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const NewFab = () => <h2>NewFab</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hey trhere</div>;
+  return (
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/fab/new" component={NewFab} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
