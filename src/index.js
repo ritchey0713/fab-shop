02b18129cae6db149b105bd85c6754d19f0ea476
Dj <ritchey0713@gmail.com>
2020-12-22 14:42:14 -0500

    import styles classes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "materialize-css/dis/css/materialize.min.css";
+
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./components/App";
