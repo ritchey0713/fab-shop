067a7a0dc0ed37e9fccb220bfbcdea7c811b511d
Dj <ritchey0713@gmail.com>
2020-12-22 14:37:23 -0500

    rename folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./header/header.component";
 
-const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const NewFab = () => <h2>NewFab</h2>;
 const Landing = () => <h2>Landing</h2>;
