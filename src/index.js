64d05b684155da5585dbfc5e7fb0fa55a3fe0a18
Dj <ritchey0713@gmail.com>
2020-12-22 14:37:40 -0500

    fix importing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import App from "./comoponents/App";
+import App from "./components/App";
 
 ReactDOM.render(
   <Provider store={store}>
