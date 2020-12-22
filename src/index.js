a2c5a3e9aa4156d2757fe27777afa34073aff783
Dj <ritchey0713@gmail.com>
2020-12-22 14:45:00 -0500

    fix typo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "materialize-css/dis/css/materialize.min.css";
+import "materialize-css/dist/css/materialize.min.css";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
