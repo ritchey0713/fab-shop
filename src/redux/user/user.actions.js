c9458708601a5a1b43faf87edbae8e9b5a74516a
Dj <ritchey0713@gmail.com>
2020-12-23 10:58:01 -0500

    flesh out fetch user action

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 import { FETCH_USER } from "./user.types";
+
 const fetchUser = () => {
-  axios.get("/api/current_user");
+  return (dispatch) => {
+    axios
+      .get("/api/current_user")
+      .then((resp) => dispatch({ type: FETCH_USER, payload: resp }));
+  };
 };
