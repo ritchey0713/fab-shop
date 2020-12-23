1aef72ae67edb8c20f4399f941305ad60d80768c
Dj <ritchey0713@gmail.com>
2020-12-23 10:36:42 -0500

    add materialize header styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-const Header = () => <div>Header</div>;
+const Header = () => (
+  <nav>
+    <div className="nav-wrapper">
+      <a href="/" className="left brand-logo">
+        Fab Shop
+      </a>
+      <ul className="right">
+        <li>
+          <a href="/">Log in with Google</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
 
 export default Header;
