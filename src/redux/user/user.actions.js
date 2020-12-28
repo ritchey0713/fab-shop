18eefb838081cdc291e0d288d9f5cf75e36fc854
Dj <ritchey0713@gmail.com>
2020-12-28 12:32:24 -0500

    refactor to es6

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { FETCH_USER } from "./user.types";
 
-export const fetchUser = () => {
-  return (dispatch) => {
-    axios
-      .get("/api/current_user")
-      .then((resp) => dispatch({ type: FETCH_USER, payload: resp }));
-  };
+export const fetchUser = () => async (dispatch) => {
+  const resp = await axios.get("/api/current_user");
+  dispatch({ type: FETCH_USER, payload: resp });
 };
+// return (dispatch) => {
+//   axios
+//     .get("/api/current_user")
+//     .then((resp) => dispatch({ type: FETCH_USER, payload: resp }));
+// };
