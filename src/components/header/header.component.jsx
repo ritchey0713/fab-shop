4117a18e4af3ece7da9d0cdd81adc6b08263b5a8
Dj <ritchey0713@gmail.com>
2020-12-28 12:38:58 -0500

    connect header to redux store

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Header = () => (
-  <nav>
-    <div className="nav-wrapper">
-      <a href="/" className="left brand-logo">
-        Fab Shop
-      </a>
-      <ul className="right">
-        <li>
-          <a href="/">Log in with Google</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+const Header = (props) => {
+  console.log(props);
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="/" className="left brand-logo">
+          Fab Shop
+        </a>
+        <ul className="right">
+          <li>
+            <a href="/">Log in with Google</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state, ownProps) => ({
+  current_user: state.user.current_user,
+});
+
+export default connect(mapStateToProps)(Header);
