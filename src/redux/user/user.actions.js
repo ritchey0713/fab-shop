2976b13cf5a9c2a16ed610192bf2868039fb3167
Dj <ritchey0713@gmail.com>
2020-12-28 12:19:08 -0500

    export fetchUser

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { FETCH_USER } from "./user.types";
 
-const fetchUser = () => {
+export const fetchUser = () => {
   return (dispatch) => {
     axios
       .get("/api/current_user")
