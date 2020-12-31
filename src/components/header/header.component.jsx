4d8e81642428286631e1cf179165f63f72573839
Dj <ritchey0713@gmail.com>
2020-12-31 11:03:41 -0500

    remove logic funtion

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,15 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import LoggedIn from "./loggedIn.component";
 import NotLoggedIn from "./NotLoggedIn.component";
-
-const isLoggedIn = (currentUser) => {
-  if (currentUser === null) {
-    return "LOADING COMP";
-  } else if (!currentUser) {
-    return <NotLoggedIn />;
-  }
-  return <LoggedIn />;
-};
+import { isLoggedIn } from "../../utils/isLoggedIn";
 
 const Header = ({ currentUser }) => {
   return (
