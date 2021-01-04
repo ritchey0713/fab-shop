294a2b2f2a20ad8725479636932d3b31b7819215
Dj <ritchey0713@gmail.com>
2021-01-04 14:19:47 -0500

    create landing file

---
+++ src/components/landing/Landing.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Landing = () => (
+  <div style={{ textAlign: "center" }}>
+    <h1>Fab Shop Connect!</h1>
+    Present a ticket, and vote on others to get your fab needs fulfilled!
+  </div>
+);
+
+export default Landing;
