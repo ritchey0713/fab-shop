f89ba6d8a6846f3432473e5af28816194e23ab0c
Dj <ritchey0713@gmail.com>
2021-01-07 13:52:13 -0500

    fix env call

--- src/components/checkout/checkout.container.jsx
+++ src/components/checkout/checkout.container.jsx
@@ -3,9 +3,10 @@ import { useHistory } from "react-router-dom";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./checkout.component";
+import GlobalStyles from "./prebuilt/GlobalStyles";
 import Shop from "./prebuilt/Shop";
 
-const stripePromise = loadStripe(process.env.PUBLISHABLLE_KEY);
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 const CheckoutContainer = () => {
   let history = useHistory();
@@ -19,6 +20,7 @@ const CheckoutContainer = () => {
 
   return (
     <Elements stripe={stripePromise}>
+      <GlobalStyles />
       <Shop onAdd={add} onRemove={rem} numItem={numItem} />
       <CheckoutForm
         price={getPrice(numItem)}
