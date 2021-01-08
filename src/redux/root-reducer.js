92b001eb055f8099328b937483734efb3676bf8c
Dj <ritchey0713@gmail.com>
2021-01-08 16:11:52 -0500

    add orders reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
+import orderReducer from "./order/order.reducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  order: orderReducer,
 });
 
 export default rootReducer;
