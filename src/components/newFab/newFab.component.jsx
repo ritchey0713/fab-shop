25eb674076594c5421221eb91618a9783775e9da
Dj <ritchey0713@gmail.com>
2021-01-11 15:57:49 -0500

    build form with formik and yup

--- src/components/newFab/newFab.component.jsx
+++ src/components/newFab/newFab.component.jsx
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import * as yup from "yup";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
+import CustomCheckbox from "../formInputs/checkBox.component";
+import CustomSelect from "../formInputs/customSelect.component";
+import CustomTextField from "../formInputs/textField.component";
+import CustomDatePicker from "../formInputs/datePicker.component";
 
 // title
 //date needed
@@ -14,10 +18,19 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 let fabSchema = yup.object().shape({
   title: yup.string().max(255, "Too long!").required("Required"),
   dateNeeded: yup.date().required("Required"),
-  useage: yup.string.required("Required"),
-  material: yup.string().max(50, "Too long!").required("Required"),
-  isStructural: yup.boolean().required("Required"),
-  units: yup.number().required("Required"),
+  useage: yup.string().required("Required"),
+  material: yup
+    .string()
+    .oneOf(
+      ["aluminum", "mild steel", "other", "stainless steel"],
+      "Must select a material"
+    )
+    .required("required"),
+  units: yup
+    .number()
+    .min(1, "must make at least one!")
+    .max(20, "please email us to order")
+    .required("Required"),
 });
 
 const NewFabForm = () => {
@@ -40,14 +53,83 @@ const NewFabForm = () => {
           console.log("ACTIONS:", actions);
         }}
       >
-        <Form>
-          <h2>Add new fab survey</h2>
-          <div>
-            <label htmlFor="title">Title</label>
-            <Field />
-          </div>
-        </Form>
+        {() => {
+          return (
+            <Form>
+              <h2>Add new fab survey</h2>
+              <div>
+                <CustomTextField
+                  label="Title"
+                  name="title"
+                  type="text"
+                  placeholder="Fab item name"
+                />
+              </div>
+
+              <div>
+                <CustomDatePicker
+                  label="Date needed by"
+                  name="dateNeeded"
+                  type="date"
+                />
+              </div>
+
+              <div>
+                <CustomTextField
+                  label="Price willing to pay"
+                  name="price"
+                  type="number"
+                />
+              </div>
+
+              <div>
+                <CustomTextField
+                  label="Part Purpose"
+                  name="useage"
+                  placeholder="The part is for..."
+                />
+              </div>
+
+              <div>
+                <CustomSelect name="material">
+                  <option value="">Please select a material</option>
+                  <option value="aluminum">aluminum</option>
+                  <option value="mild steel">mild steel</option>
+                  <option value="stainless steel">stainless steel</option>
+                  <option value="other">other</option>
+                </CustomSelect>
+              </div>
+
+              <div>
+                <CustomCheckbox name="isStructural">
+                  Is this a structural piece?
+                </CustomCheckbox>
+              </div>
+
+              <div>
+                <CustomTextField
+                  label="Units needed"
+                  name="units"
+                  type="number"
+                />
+              </div>
+
+              <div>
+                <CustomTextField
+                  label="Special Notes"
+                  name="notes"
+                  type="textarea"
+                  className="materialize-textarea"
+                />
+              </div>
+
+              <button type="submit">Send Fab Ticket</button>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
 };
+
+export default NewFabForm;
