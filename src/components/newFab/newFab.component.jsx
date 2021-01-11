591989905f42a5aa51c7135036ac924e32a3da9f
Dj <ritchey0713@gmail.com>
2021-01-11 11:38:56 -0500

    add form and fields

--- src/components/newFab/newFab.component.jsx
+++ src/components/newFab/newFab.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
@@ -9,10 +9,45 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 // material
 // is it structural
 // units needed
+// special notes
 
 let fabSchema = yup.object().shape({
   title: yup.string().max(255, "Too long!").required("Required"),
   dateNeeded: yup.date().required("Required"),
   useage: yup.string.required("Required"),
   material: yup.string().max(50, "Too long!").required("Required"),
+  isStructural: yup.boolean().required("Required"),
+  units: yup.number().required("Required"),
 });
+
+const NewFabForm = () => {
+  const initialValues = {
+    title: "",
+    dateNeeded: "",
+    price: 0,
+    useage: "",
+    material: "",
+    isStructural: false,
+    units: 0,
+  };
+  return (
+    <div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={fabSchema}
+        onSubmit={(values, actions) => {
+          console.log("VALUES:", values);
+          console.log("ACTIONS:", actions);
+        }}
+      >
+        <Form>
+          <h2>Add new fab survey</h2>
+          <div>
+            <label htmlFor="title">Title</label>
+            <Field />
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
