04251af2e1386c44b24bb0a11c9edd2bb513602c
Dj <ritchey0713@gmail.com>
2021-01-12 16:47:16 -0500

    comment out persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,9 +2,17 @@ import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import orderReducer from "./order/order.reducer";
 
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["user"],
+// };
+
 const rootReducer = combineReducers({
   user: userReducer,
   order: orderReducer,
 });
+
+// export default rootReducer;
 
 export default rootReducer;
