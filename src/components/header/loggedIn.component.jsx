146abb1b56101f96bf43f5563f324ce8dd6736f6
Dj <ritchey0713@gmail.com>
2021-01-14 10:52:54 -0500

    add redirect

--- src/components/header/loggedIn.component.jsx
+++ src/components/header/loggedIn.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { logoutUser } from "../../redux/user/user.actions";
 
 const LoggedIn = ({ logoutUser }) => {
+  const history = useHistory();
   const buttonOnClick = () => {
     logoutUser();
+    history.push("/");
   };
 
   return (
