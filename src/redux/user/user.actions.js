94d10ca8b9b4c0e9f962c71489be0c9f15a140be
Dj <ritchey0713@gmail.com>
2021-01-14 10:52:22 -0500

    add new logging in action (dead in with passport)

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -14,3 +14,28 @@ export const logoutUser = () => async (dispatch) => {
   await axios.get("/api/logout");
   dispatch({ type: LOG_OUT_USER });
 };
+
+export const signIn = () => async (dispatch) => {
+  let params = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Headers": "X-Requested-With,content-type",
+    },
+    referrerPolicy: "no-referrer-when-downgrade",
+    withCredientials: true,
+  };
+  const resp = await fetch("/auth/google", { credentials: "include" });
+  // const resp = await axios({
+  //   method: "GET",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Access-Control-Allow-Origin": "http://localhost:3000",
+  //     "Access-Control-Allow-Headers": "X-Requested-With,content-type",
+  //   },
+  //   url: "/auth/google",
+  //   withCredentials: true,
+  // });
+  console.log(resp);
+};
