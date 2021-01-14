6c62625760007eff4879c3c4c7af021c13720d38
Dj <ritchey0713@gmail.com>
2021-01-14 10:52:47 -0500

    revert log in link

--- src/components/header/NotLoggedIn.component.jsx
+++ src/components/header/NotLoggedIn.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { signIn } from "../../redux/user/user.actions";
 
-const NotLoggedIn = () => (
-  <li>
-    <a href="/auth/google">LOG IN WITH GOOGLE</a>
-  </li>
+const NotLoggedIn = ({ signIn }) => (
+  <a href="/auth/google">LOG IN WITH GOOGLE</a>
 );
 
-export default NotLoggedIn;
+export default connect(null, { signIn })(NotLoggedIn);
