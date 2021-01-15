fe0c2e0cc3c4cf3eadd85992a4faa49b71209526
Dj <ritchey0713@gmail.com>
2021-01-15 16:16:10 -0500

    add basic fabs

---
+++ src/redux/fab/fab.reducer.js
@@ -0,0 +1,22 @@
+import { CREATE_FAB_REQUEST } from "./fab.types";
+
+const INITIAL_STATE = {
+  requests: {},
+  errors: {},
+};
+
+const fabReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CREATE_FAB_REQUEST:
+      return Object.assign({}, state, {
+        requests: {
+          ...state.requests,
+          [action.payload._id]: action.payload,
+        },
+      });
+    default:
+      return state;
+  }
+};
+
+export default fabReducer;
