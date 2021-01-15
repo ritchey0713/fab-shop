434d547bcc7a59275300c29ff557f2c0148ebc66
Dj <ritchey0713@gmail.com>
2021-01-15 16:15:36 -0500

    reset link

--- src/components/header/NotLoggedIn.component.jsx
+++ src/components/header/NotLoggedIn.component.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import { signIn } from "../../redux/user/user.actions";
 
-const NotLoggedIn = ({ signIn }) => (
-  <a href="/auth/google">LOG IN WITH GOOGLE</a>
-);
+const NotLoggedIn = ({ signIn }) => {
+  return <a href="/auth/google">LOG IN WITH GOOGLE</a>;
+};
 
 export default connect(null, { signIn })(NotLoggedIn);
