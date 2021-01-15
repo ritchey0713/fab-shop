eb1f01ea8a3d4f6783baab2960ca253f3fce10e9
Dj <ritchey0713@gmail.com>
2021-01-15 16:16:18 -0500

    refactor reduxer

--- src/redux/order/order.reducer.js
+++ src/redux/order/order.reducer.js
@@ -1,14 +1,19 @@
 import { SEND_PAYMENT } from "./order.types";
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  invoices: {},
+};
 
 const orderReducer = (state = INITIAL_STATE, action) => {
+  console.log(action.payload);
   switch (action.type) {
     case SEND_PAYMENT:
-      return {
-        ...state,
-        [action.payload.id]: action.payload,
-      };
+      return Object.assign({}, state, {
+        invoices: {
+          ...state.invoices,
+          [action.payload._id]: action.payload,
+        },
+      });
     default:
       return state;
   }
