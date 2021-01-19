ec84faa89d8b7cdf5a79e18a4af81351659bc80d
Dj <ritchey0713@gmail.com>
2021-01-19 13:07:47 -0500

    dispatch fab request

--- src/redux/fab/fab.actions.js
+++ src/redux/fab/fab.actions.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+// import { browserHistory } from "react-router-dom";
 import { CREATE_FAB_REQUEST } from "./fab.types";
 
 export const createRequest = (requestData) => async (dispatch) => {
   const resp = await axios.post("/api/requests", requestData);
   console.log(resp);
+  dispatch({ type: CREATE_FAB_REQUEST, payload: resp.data });
+  // browserHistory.push("/dashboard");
 };
