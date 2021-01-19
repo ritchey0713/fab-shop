b220d2275d8abf465e32798880924eadcead19a2
Dj <ritchey0713@gmail.com>
2021-01-19 11:45:10 -0500

    add log for testing

--- src/redux/fab/fab.actions.js
+++ src/redux/fab/fab.actions.js
@@ -2,5 +2,6 @@ import axios from "axios";
 import { CREATE_FAB_REQUEST } from "./fab.types";
 
 export const createRequest = (requestData) => async (dispatch) => {
-  const resp = await axios.post("/api/requests");
+  const resp = await axios.post("/api/requests", requestData);
+  console.log(resp);
 };
