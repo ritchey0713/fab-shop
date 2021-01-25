c405d17157bd81ca9e34418a1dd3d3de673a8dc3
Dj <ritchey0713@gmail.com>
2021-01-25 09:38:41 -0500

    add new case

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,4 +1,4 @@
-import { FETCH_USER, LOG_OUT_USER } from "./user.types";
+import { ADD_CREDITS, FETCH_USER, LOG_OUT_USER } from "./user.types";
 
 const INITIAL_STATE = {
   currentUser: null,
@@ -6,8 +6,10 @@ const INITIAL_STATE = {
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
+  console.log(action);
   switch (action.type) {
     case FETCH_USER:
+    case ADD_CREDITS:
       return {
         ...state,
         currentUser: action.payload || false,
