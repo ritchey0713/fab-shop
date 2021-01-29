59435f04ccf45ee84b21f54411413ad5a670baf8
Dj <ritchey0713@gmail.com>
2021-01-29 09:39:14 -0500

    add axios to window to test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import App from "./components/App";
 
+import axios from "axios";
+
+// test emails
+window.axios = axios;
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
