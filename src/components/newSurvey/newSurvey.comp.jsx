a5c9d457c2bb7cb80b0e401ec386dafb0719ae96
Dj <ritchey0713@gmail.com>
2021-02-02 12:38:09 -0500

    add form, validations, and start button for more recipients

--- src/components/newSurvey/newSurvey.comp.jsx
+++ src/components/newSurvey/newSurvey.comp.jsx
@@ -1,5 +1,5 @@
 import { Formik, Form } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import * as yup from "yup";
 import CustomTextField from "../formInputs/textField.component";
@@ -25,36 +25,76 @@ let surveySchema = yup.object().shape({
 
 const SurveyForm = () => {
   const history = useHistory();
+  const [recipientCount, setRecipientCount] = useState(1);
+  const initialValues = {
+    title: "",
+    body: "",
+    subject: "",
+    recipients: "",
+  };
 
   return (
     <div>
-      <Form>
-        <h2>Create an email survey!</h2>
-        <div>
-          <CustomTextField
-            label="title"
-            name="title"
-            type="text"
-            placeholder="Title"
-          />
-        </div>
-        <div>
-          <CustomTextField
-            label="subject"
-            name="subject"
-            type="text"
-            placeholder="Email Subject"
-          />
-        </div>
-        <div>
-          <CustomTextField
-            label="body"
-            name="body"
-            type="text"
-            placeholder="Email Body"
-          />
-        </div>
-      </Form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={surveySchema}
+        onSubmit={(values, action) => {
+          console.log("SUBMITTED");
+        }}
+      >
+        {() => (
+          <div>
+            <Form>
+              <h2>Create an email survey!</h2>
+              <div>
+                <CustomTextField
+                  label="title"
+                  name="title"
+                  type="text"
+                  placeholder="Title"
+                />
+              </div>
+              <div>
+                <CustomTextField
+                  label="subject"
+                  name="subject"
+                  type="text"
+                  placeholder="Email Subject"
+                />
+              </div>
+              <div>
+                <CustomTextField
+                  label="body"
+                  name="body"
+                  type="text"
+                  placeholder="Email Body"
+                />
+              </div>
+              <div>
+                <CustomTextField
+                  label="recipients"
+                  name="recipients"
+                  type="text"
+                  placeholder="recipient email"
+                />
+              </div>
+              <div>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setRecipientCount(recipientCount + 1);
+                    console.log(recipientCount);
+                  }}
+                >
+                  ADD RECIPIENT
+                </button>
+              </div>
+            </Form>
+          </div>
+        )}
+      </Formik>
     </div>
   );
 };
+
+export default SurveyForm;
