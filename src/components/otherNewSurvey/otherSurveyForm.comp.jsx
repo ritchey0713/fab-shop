00370f99a0b011fe39d2a1e2deb9a53a5c36f5b2
Dj <ritchey0713@gmail.com>
2021-02-09 12:46:10 -0500

    redo based on docs page

--- src/components/otherNewSurvey/otherSurveyForm.comp.jsx
+++ src/components/otherNewSurvey/otherSurveyForm.comp.jsx
@@ -1,7 +1,89 @@
+// // form page
+
+// import React, { useState } from "react";
+// import { reduxForm, Field, Fields, FieldArray } from "redux-form";
+// import BasicTextField from "./basicTextField.comp";
+// import emailField from "./emailField.comp";
+
+// const FIELDS = [
+//   { label: "Survey Title", name: "title" },
+//   { label: "Subject", name: "subject" },
+//   { label: "Body", name: "body" },
+// ];
+
+// const OtherSurveyForm = (props) => {
+//   const [recipients, setRecipients] = useState({});
+//   const renderFields = () =>
+//     FIELDS.map(({ label, name }) => {
+//       return (
+//         <Field
+//           key={name}
+//           label={label}
+//           name={name}
+//           type="text"
+//           component={BasicTextField}
+//         />
+//       );
+//     });
+
+//   const renderSelectDropdown = () => {
+//     let selects = [];
+//     for (let i = 0; i <= 100; i++) {
+//       selects.push(<option value={i}>{i}</option>);
+//     }
+//     return selects;
+//   };
+
+//   const handleSelectChange = (e) => {
+//     setRecipients([]);
+//     for (let i = 1; i <= e.target.value; i++) {
+//       setRecipients((recipients) => [...recipients, {}]);
+//     }
+//     //renderEmailFields();
+//   };
+
+//   // const renderEmailFields = () =>
+//   //   recipients.length > 0 ? (
+//   //     <Fields names={recipients} label="Email" component={emailField} />
+//   //   ) : null;
+
+//   const renderEmailFields = () =>
+//     recipients.length > 0 ? (
+//       <FieldArray
+//         name="recipients"
+//         fields={recipients}
+//         label="Email"
+//         component={emailField}
+//       />
+//     ) : null;
+
+//   return (
+//     <div>
+//       <form onSubmit={props.handleSubmit((values) => console.log(values))}>
+//         {renderFields()}
+//         <select onChange={handleSelectChange} style={{ display: "block" }}>
+//           {renderSelectDropdown()}
+//         </select>
+//         {renderEmailFields()}
+//         <button type="submit">submit</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default reduxForm({
+//   form: "surveyForm",
+// })(OtherSurveyForm);
+
+// // <button type="button" onClick={renderEmailFields()}>
+// // Add recipient
+// // </button>
+// //{renderEmailFields()}
+
 // form page
 
 import React, { useState } from "react";
-import { reduxForm, Field, Fields } from "redux-form";
+import { reduxForm, Field, Fields, FieldArray } from "redux-form";
 import BasicTextField from "./basicTextField.comp";
 import emailField from "./emailField.comp";
 
@@ -12,8 +94,7 @@ const FIELDS = [
 ];
 
 const OtherSurveyForm = (props) => {
-  const [recipientCount, setRecipientCount] = useState(1);
-  const [recipients, setRecipients] = useState([]);
+  const [recipients, setRecipients] = useState({});
   const renderFields = () =>
     FIELDS.map(({ label, name }) => {
       return (
@@ -27,35 +108,26 @@ const OtherSurveyForm = (props) => {
       );
     });
 
-  const renderSelectDropdown = () => {
-    let selects = [];
-    for (let i = 0; i <= 100; i++) {
-      selects.push(<option value={i}>{i}</option>);
-    }
-    return selects;
-  };
-
-  const handleSelectChange = (e) => {
-    setRecipients([]);
-    for (let i = 1; i <= e.target.value; i++) {
-      setRecipients((recipients) => [...recipients, `recipient-${i}`]);
-    }
-    //renderEmailFields();
-  };
+  // const renderEmailFields = () =>
+  //   recipients.length > 0 ? (
+  //     <Fields names={recipients} label="Email" component={emailField} />
+  //   ) : null;
 
   const renderEmailFields = () =>
     recipients.length > 0 ? (
-      <Fields names={recipients} label="Email" component={emailField} />
+      <FieldArray
+        name="recipients"
+        fields={recipients}
+        label="Email"
+        component={emailField}
+      />
     ) : null;
-  console.log(recipients);
+
   return (
     <div>
       <form onSubmit={props.handleSubmit((values) => console.log(values))}>
         {renderFields()}
-        <select onChange={handleSelectChange} style={{ display: "block" }}>
-          {renderSelectDropdown()}
-        </select>
-        {renderEmailFields()}
+        <FieldArray name="emails" component={emailField} />
         <button type="submit">submit</button>
       </form>
     </div>
