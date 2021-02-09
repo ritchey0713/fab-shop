cf865b0cf10b7d8d541bb8ddbfdf5244dfb84539
Dj <ritchey0713@gmail.com>
2021-02-09 10:36:07 -0500

    flesh out basic use

--- src/components/otherNewSurvey/otherSurveyForm.comp.jsx
+++ src/components/otherNewSurvey/otherSurveyForm.comp.jsx
@@ -1,13 +1,34 @@
 // form page
 
+import { FieldArray } from "formik";
 import React from "react";
-import { reduxForm, Field } from "redux-form";
+import { reduxForm, Field, Fields } from "redux-form";
+import surveyField from "./surveyField.comp";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject", name: "subject" },
+  { label: "Body", name: "body" },
+  { label: "Recipients", name: "emails" },
+];
 
 const OtherSurveyForm = (props) => {
+  const renderFields = () =>
+    FIELDS.map(({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          label={label}
+          name={name}
+          type="text"
+          component={surveyField}
+        />
+      );
+    });
   return (
     <div>
       <form onSubmit={props.handleSubmit((values) => console.log(values))}>
-        <Field type="text" name="surveyTitle" component="input" />
+        {renderFields()}
         <button type="submit">submit</button>
       </form>
     </div>
