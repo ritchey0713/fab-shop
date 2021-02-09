bc6f76bc3e30b5c9d0a45064d16d6bf26b83ff73
Dj <ritchey0713@gmail.com>
2021-02-09 09:56:32 -0500

    add wraopper

---
+++ src/components/otherNewSurvey/otherNewSurvey.comp.jsx
@@ -0,0 +1,15 @@
+// renders form and review page
+
+import React from "react";
+import OtherSurveyForm from "./otherSurveyForm.comp";
+
+const NewSurvey = () => {
+  return (
+    <div>
+      new survey
+      <OtherSurveyForm />
+    </div>
+  );
+};
+
+export default NewSurvey;
