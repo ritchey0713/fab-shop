9d79202166adc1b07a16748e28bcbb2a45f2a17c
Dj <ritchey0713@gmail.com>
2021-02-09 12:46:22 -0500

    needs to be refactored, too verbouse

--- src/components/otherNewSurvey/emailField.comp.jsx
+++ src/components/otherNewSurvey/emailField.comp.jsx
@@ -1,18 +1,33 @@
 //label and text input
-
 import React from "react";
+import { Field } from "redux-form";
+import EmailTemplate from "./emailTemplate.comp";
+// export default (props) => {
+//   console.log(props.names);
+//   return props.names.map((name) => {
+//     return (
+//       <div>
+//         <label>{props.label}</label>
+//         <input onBlur={props[name].input.onBlur} type="text" />
+//       </div>
+//     );
+//   });
+// };
 
-export default (props) => {
-  console.log(props.names);
-  return props.names.map((name) => {
-    return (
-      <div>
-        <label>{props.label}</label>
-        <input onBlur={props[name].input.onBlur} type="text" />
-      </div>
-    );
-  });
-};
+// export default (props) => {
+//   return props.fields.map((member, index) => {
+//     debugger;
+//     return <Field name={member} label={"Email"} component={EmailTemplate} />;
+//   });
+// };
+// return props.names.map((name) => {
+//   return (
+//     <div>
+//       <label>{props.label}</label>
+//       <input onBlur={props[name].input.onBlur} type="text" />
+//     </div>
+//   );
+// });
 
 //{
 //   props.names.map((name) => (
@@ -22,3 +37,22 @@ export default (props) => {
 //     </div>
 //   ));
 // }
+
+const emailField = (props) => (
+  <div>
+    <button type="button" onClick={() => props.fields.push({})}>
+      add email
+    </button>
+
+    {props.fields.map((email, index) => (
+      <Field
+        name={`${email}.email`}
+        type="text"
+        component={EmailTemplate}
+        label="email"
+      />
+    ))}
+  </div>
+);
+
+export default emailField;
