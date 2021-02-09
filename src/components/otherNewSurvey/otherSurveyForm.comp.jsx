de05ba867358e4eb0faee42789f7967f01ad4948
Dj <ritchey0713@gmail.com>
2021-02-09 12:01:17 -0500

    git add build render for select to email forms

--- src/components/otherNewSurvey/otherSurveyForm.comp.jsx
+++ src/components/otherNewSurvey/otherSurveyForm.comp.jsx
@@ -1,6 +1,5 @@
 // form page
 
-import { FieldArray } from "formik";
 import React, { useState } from "react";
 import { reduxForm, Field, Fields } from "redux-form";
 import BasicTextField from "./basicTextField.comp";
@@ -10,17 +9,11 @@ const FIELDS = [
   { label: "Survey Title", name: "title" },
   { label: "Subject", name: "subject" },
   { label: "Body", name: "body" },
-  { label: "Recipients", name: "emails" },
 ];
 
 const OtherSurveyForm = (props) => {
   const [recipientCount, setRecipientCount] = useState(1);
-  const [recipients, setRecipients] = useState([
-    {
-      label: "recipients",
-      name: `recipients`,
-    },
-  ]);
+  const [recipients, setRecipients] = useState([]);
   const renderFields = () =>
     FIELDS.map(({ label, name }) => {
       return (
@@ -34,35 +27,35 @@ const OtherSurveyForm = (props) => {
       );
     });
 
+  const renderSelectDropdown = () => {
+    let selects = [];
+    for (let i = 0; i <= 100; i++) {
+      selects.push(<option value={i}>{i}</option>);
+    }
+    return selects;
+  };
+
+  const handleSelectChange = (e) => {
+    setRecipients([]);
+    for (let i = 1; i <= e.target.value; i++) {
+      setRecipients((recipients) => [...recipients, `recipient-${i}`]);
+    }
+    //renderEmailFields();
+  };
+
   const renderEmailFields = () =>
-    recipients.map(({ label, name }) => (
-      <Field
-        key={Math.random() * 110}
-        label={label}
-        name={name}
-        type="email"
-        component={emailField}
-      />
-    ));
+    recipients.length > 0 ? (
+      <Fields names={recipients} label="Email" component={emailField} />
+    ) : null;
+  console.log(recipients);
   return (
     <div>
       <form onSubmit={props.handleSubmit((values) => console.log(values))}>
         {renderFields()}
+        <select onChange={handleSelectChange} style={{ display: "block" }}>
+          {renderSelectDropdown()}
+        </select>
         {renderEmailFields()}
-        <button
-          type="button"
-          onClick={() =>
-            setRecipients([
-              ...recipients,
-              {
-                label: "recipients",
-                name: `recipients`,
-              },
-            ])
-          }
-        >
-          Add recipient
-        </button>
         <button type="submit">submit</button>
       </form>
     </div>
@@ -72,3 +65,8 @@ const OtherSurveyForm = (props) => {
 export default reduxForm({
   form: "surveyForm",
 })(OtherSurveyForm);
+
+// <button type="button" onClick={renderEmailFields()}>
+// Add recipient
+// </button>
+//{renderEmailFields()}
