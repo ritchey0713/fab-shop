e5b66da0aae4c101c6d9b0c354f7324601cdc1cd
Dj <ritchey0713@gmail.com>
2021-02-10 11:05:48 -0500

    add styling to error messagers

--- src/components/otherNewSurvey/basicTextField.comp.jsx
+++ src/components/otherNewSurvey/basicTextField.comp.jsx
@@ -2,11 +2,14 @@
 
 import React from "react";
 
-export default ({ input, label }) => {
+export default ({ input, label, meta: { touched, error } }) => {
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
+      <input {...input} style={{ marginBottom: "7px" }} />
+      <div className="red-text" style={{ marginBottom: "20px" }}>
+        {touched && error}
+      </div>
     </div>
   );
 };
