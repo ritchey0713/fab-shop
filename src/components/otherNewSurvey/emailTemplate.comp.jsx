8fd2c9acea1a602c65a6ee0fd3a558f20ef209ef
Dj <ritchey0713@gmail.com>
2021-02-10 11:05:30 -0500

    add error messages

--- src/components/otherNewSurvey/emailTemplate.comp.jsx
+++ src/components/otherNewSurvey/emailTemplate.comp.jsx
@@ -1,8 +1,11 @@
-const emailTemplate = ({ label, input }) => {
+const emailTemplate = ({ label, input, meta: { touched, error } }) => {
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      <div className="red-text" style={{ marginBottom: "20px" }}>
+        {touched && error}
+      </div>
     </div>
   );
 };
