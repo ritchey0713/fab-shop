b566f90189a79d4266d9d4d53a3cadb6c35aa51a
Dj <ritchey0713@gmail.com>
2021-02-10 11:33:28 -0500

    check emails singularity

--- src/utils/emailValidator.js
+++ src/utils/emailValidator.js
@@ -1,3 +1,24 @@
-export default (emailsArr) => {
-  emailsArr.map((email) => email.trim());
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// export default (emailsArr) => {
+//   console.log(emailsArr);
+//   if (emailsArr.length) {
+//     const invalidEmails = emailsArr
+//       .map((email) => email.trim())
+//       .filter((email) => re.test(email) === false);
+
+//     if (invalidEmails.length) {
+//       return `These emails are invalid: ${invalidEmails}`;
+//     }
+//   }
+
+//   return;
+// };
+
+export default (email) => {
+  const emailCheck = email.trim();
+
+  if (re.test(emailCheck) === false) {
+    return "Incorrect format please check email";
+  }
 };
