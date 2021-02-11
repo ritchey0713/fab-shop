bd9826b04ef19bb325eeb3dd90d8bd0e5717dc33
Dj <ritchey0713@gmail.com>
2021-02-11 11:53:29 -0500

    dynamic render review inputs

--- src/components/otherNewSurvey/formReview.comp.jsx
+++ src/components/otherNewSurvey/formReview.comp.jsx
@@ -1,14 +1,57 @@
 import React from "react";
+import { connect } from "react-redux";
+import { formFields } from "./formFields.js";
 
-const SurveyReview = (props) => {
+const SurveyReview = ({ onCancel, formValues }) => {
+  const reviewFields = () =>
+    formFields.map(({ label, name }) => (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    ));
+
   return (
     <div>
-      <p>list the review</p>
-      <button className="yellow darken-3 btn-flat" onClick={props.onCancel}>
+      <h4>Please review your survey</h4>
+      <div>
+        {reviewFields()}
+
+        <div>
+          <label>Recipients</label>
+          {formValues.emails.map((recip) => (
+            <div>{recip.email}</div>
+          ))}
+        </div>
+      </div>
+      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
     </div>
   );
 };
 
-export default SurveyReview;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    formValues: state.form.surveyForm.values,
+  };
+};
+
+export default connect(mapStateToProps)(SurveyReview);
+
+{
+  /* <div>
+          <label>Survey Title</label>
+          <div>{formValues.title}</div>
+        </div>
+
+        <div>
+          <label>Subject Line</label>
+          <div>{formValues.subject}</div>
+        </div>
+
+        <div>
+          <label>Email Body</label>
+          <div>{formValues.body}</div>
+        </div> */
+}
