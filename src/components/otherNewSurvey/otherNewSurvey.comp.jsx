41b05dc9822091646fcff6d1fa72eb659f984e23
Dj <ritchey0713@gmail.com>
2021-02-11 10:02:16 -0500

    create helper to toggle to review page

--- src/components/otherNewSurvey/otherNewSurvey.comp.jsx
+++ src/components/otherNewSurvey/otherNewSurvey.comp.jsx
@@ -1,13 +1,24 @@
 // renders form and review page
 
-import React from "react";
+import React, { useState } from "react";
 import OtherSurveyForm from "./otherSurveyForm.comp";
+import SurveyForm from "./formReview.comp";
+import SurveyReview from "./formReview.comp";
 
 const NewSurvey = () => {
+  const [showReview, setShowReview] = useState(false);
+
+  const renderReview = () => {
+    if (showReview) {
+      return <SurveyReview />;
+    }
+
+    return <OtherSurveyForm onSurveySubmit={() => setShowReview(true)} />;
+  };
   return (
     <div>
       new survey
-      <OtherSurveyForm />
+      {renderReview()}
     </div>
   );
 };
