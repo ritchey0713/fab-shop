0e912895b6b368b1cdaea2d3fba06b0f6ce325ec
Dj <ritchey0713@gmail.com>
2021-02-17 10:22:00 -0500

    add redirect route

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Header from "./header/header.component";
 import { connect } from "react-redux";
 import { fetchUser } from "../redux/user/user.actions";
@@ -7,6 +7,7 @@ import Landing from "./landing/Landing.component";
 import CheckoutForm from "./checkout/checkout.container";
 import NewFabForm from "./newFab/newFab.component";
 import PrivateRoute from "../routers/PrivateRoute";
+import RedirectRoute from "../routers/RedirectRoute";
 import Dashboard from "./dashboard/Dashboard";
 import BuyCredits from "./buyCredits/BuyCredits.container";
 import SurveyForm from "./newSurvey/newSurvey.comp";
@@ -23,12 +24,20 @@ const App = (props) => {
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route exact path="/" component={Landing} />
           <PrivateRoute
+            exact
             authed={currentUser}
             path="/dashboard"
             component={Dashboard}
           />
+          <RedirectRoute
+            exact
+            path="/"
+            redirectRoute="/dashboard"
+            authed={currentUser}
+            component={Landing}
+          />
+
           <Route exact path="/fab/new" component={NewFabForm} />
           <PrivateRoute
             path="/checkout"
