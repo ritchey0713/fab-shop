a712ffbf6af0c209a3c3c258f4b29ec79f337b18
Dj <ritchey0713@gmail.com>
2021-02-22 11:57:29 -0500

    flesh out survey list

--- src/components/surveyList/SurveyList.comp.jsx
+++ src/components/surveyList/SurveyList.comp.jsx
@@ -2,14 +2,38 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchSurveys } from "../../redux/survey/survey.actions";
 
-const SurveyList = ({ fetchSurveys }) => {
+const SurveyList = ({ fetchSurveys, surveys }) => {
   useEffect(() => {
     fetchSurveys();
   }, [fetchSurveys]);
 
-  return <div>Survey list</div>;
+  const renderSurveys = () => {
+    return Object.values(surveys)
+      .reverse()
+      .map((survey) => (
+        <div className="card blue-grey darken-1" key={survey._id}>
+          <div className="card-content white-text">
+            <span className="card-title">{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className="right">
+              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      ));
+  };
+
+  return <div>Survey list {surveys ? renderSurveys() : "No Surveys"}</div>;
 };
 
-const mapStateToProps = ({ surveys }) => surveys;
+const mapStateToProps = ({ surveys }) => {
+  return {
+    surveys: Object.values(surveys),
+  };
+};
 
 export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
