add04e1a13021c724a2e46b0bf7eec44ef6a9454
Dj <ritchey0713@gmail.com>
2021-02-22 10:08:42 -0500

    remove css colors

--- src/components/prebuilt/Row.component.jsx
+++ src/components/prebuilt/Row.component.jsx
@@ -3,10 +3,8 @@ import styled from "@emotion/styled";
 const Row = styled.div`
   width: 475px;
   margin: 30px auto;
-  box-shadow: 0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08),
-    inset 0 1px 0 #829fff;
   border-radius: 4px;
-  background-color: #7795f8;
+  ${"" /* background-color: #7795f8; */}
   position: relative;
 `;
 
