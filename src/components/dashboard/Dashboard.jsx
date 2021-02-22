f9da20bb9bcaa9710aaf236dd73b44faf7dcce2a
Dj <ritchey0713@gmail.com>
2021-02-22 11:14:05 -0500

    render survey list

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import SurveyList from "../surveyList/SurveyList.comp";
+
 const Dashboard = () => {
   return (
     <div>
-      DASHBOARD
+      <SurveyList />
       <div className="fixed-action-btn">
         <Link to="survey/new" className="btn-floating btn-large blue">
           <i className="material-icons">add</i>
